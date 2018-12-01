0d029c44e97ee20cbb9275cff2e05036f19658d7
StanislavFedyna <stanislavfedyna@gmail.com>
2018-12-01 20:34:18 +0200

    added 5 questions and 5 answers for main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var questions = [
+  new Question('Which one is not an object oriented programming language?',['Java','C#','C++','C'], 'C'),
+  new Question('Which language is used for styling web pages?',['HTML','jQuery','CSS','XML'], 'CSS'),
+  new Question('There are ____ main components of object oriented programing?',['1','6','2','4'], '4'),
+  new Question('Which language is used for web apps?',['PHP','Python','JavaScript','ALL'], 'ALL'),
+  new Question('MVC is a _____.?',['Language','Library','Framework','ALL'], 'Framework'),
+];
