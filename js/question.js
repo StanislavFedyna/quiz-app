bbe2308cf10df57b79954890a53cc1bf48cfae83
StanislavFedyna <stanislavfedyna@gmail.com>
2018-12-01 20:42:59 +0200

    fixed problem with questions

--- js/question.js
+++ js/question.js
@@ -1,9 +1,9 @@
-function Question(text,choices,answer){
+function Question(text, choices, answer) {
   this.text = text;
   this.choices = choices;
   this.answer = answer;
 }
 
-Question.prototype.correctAnswer = function(choice){
-  return choice === this.answer;
+Question.prototype.isCorrectAnswer = function(choice) {
+  return this.answer === choice;
 }
--- js/quiz_controller.js
+++ js/quiz_controller.js
@@ -1,20 +1,21 @@
-function Quiz(questions){
+function Quiz(questions) {
   this.score = 0;
   this.questions = questions;
   this.questionIndex = 0;
 }
 
-Quiz.prototype.getQuestionIndex = function(){
+Quiz.prototype.getQuestionIndex = function() {
   return this.questions[this.questionIndex];
 }
 
-Quiz.prototype.isEnded = function(){
-  return this.questions.length === this.questionIndex;
+Quiz.prototype.guess = function(answer) {
+  if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
+  }
+
+  this.questionIndex++;
 }
 
-Quiz.prototype.guess = function(answer){
-  this.questionIndex++;
-  if(this.questionIndex().correctAnswer(answer)){
-    this.score++;
-  }
+Quiz.prototype.isEnded = function() {
+  return this.questionIndex === this.questions.length;
 }
